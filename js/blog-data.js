a67e3c4763d311eed5d2db124b212a82c9a168b5
NeDi Sopian <nedisopian@gmail.com>
2026-01-26 14:12:30 +0700

    blog-baru

--- js/blog-data.js
+++ js/blog-data.js
@@ -64,12 +64,30 @@ const blogData = [
             <p>Padi bukan hanya sebagai bahan pokok semata, dalam pandangan Tradisi Masyarakat Gelaralam, padi merupakan Makhluk yang sama seperti manusia. Tak ayal proses penanaman padi di Kasepuhan Gelaralam seperti hal nya kita mengurusi Manusia dari mulai kecil, dewasa hingga akhirnya menikah dan beranak. “Rurukan berarti merawat, berasal dari kata Ruruk yang artinya memomong,” Aki Koyod.</p>
             <p>Tradisi menanam Padi satu tahun sekali masih mereka pertahankan hingga saat ini, terhitung 160 variates padi masih mereka pertahankan sejak 1368 hingga saat ini. Dalam proses padi menjadi beras, mereka masih menggunakan Tradisi lama, dari mulai Ngaseuk atau menanam padi Huma Ladang yang memakai metode tongkat untuk melubangi tanah, hingga memakai Ani-ani sebagai alat untuk memotong padi dari tangkai nya ketika masa panen telah tiba. Tradisi ini merupakan warisan yang wajib dijaga oleh Masyarakat Gelaralam, karena sesuai dengan amanat Leluhur mereka jika “Kudu bisa ngigelan jeung ngigelkeun jaman, tapi ulah kababawa ku jaman,” yang berarti Kita harus bisa beradaptasi akan perkembangan jaman, tapi jangan sesekali tergerus oleh jaman.</p>
             <p>Tidak seperti di Kanekes Baduy dimana anak kecil disana tidak diwajibkan untuk sekolah, Masyarakat Gelaralam membuka dirinya dengan hal berbau Teknologi, digital, dan Pendidikan. Mereka biasa bersekolah hingga Sekolah Menengah Atas untuk selanjutnya mereka dapat meilih antara bekerja di Kota atau kembali menjadi seorang Petani dan penggarap Kebun. Dapat disimpulkan jika Masyarakat Kasepuhan dapat hidup dalam keseimbangan menjaga Tradisi dan juga tidak menutup akan perkambangan jaman yang bersifat masivve. (TAMA)</p>
-            
         `,
         day: 23,
         month: 'Juli',
         year: 2025,
         image: 'assets/blog/kampung-padi.jpg',
         category: 'STORY'
-    }
+    },
+    {
+        id: 5,
+        title: 'ANGKLUNG DAN PADI',
+        description: 'Angklung adalah salahsatu Kesenian dari Tatar Sunda dimana bahan utama Angklung adalah Bambu yang dirangkai sedemikian rupa dan pola membentuk tangga nada Da, Mi, Na, Ti, La, Da.',
+        content: `
+        <p>Angklung adalah salahsatu Kesenian dari Tatar Sunda dimana bahan utama Angklung adalah Bambu yang dirangkai sedemikian rupa dan pola membentuk tangga nada Da, Mi, Na, Ti, La, Da. Pada saat ini kita Mengenal Angklung lewat peran Kang Udjo, seorang Maestro seni Sunda yang berhasil membawa Angklung dikenal oleh Khalayak umum, bahkan disahkan oleh UNESCO sebagai warisan budaya dunia dari Indonesia. Namun, tak banyak orang tahu bahwa di Banten Kidul Angklung merupakan Kesenian Sakral yang erat hubungannya dengan sosok Dewi Sri atau yang kita kenal sebagai Padi dalam Mitologi Sunda Kuna. Peran angklung tidak semata-mata ada pada setiap ritual yang dilaksanakan oleh masyarakat adat Banten Kidul, karena dalam pandangan masyarakat adat Banten Kidul, kayu dalam angklung adalah manifestasi dari apa yang dinamakan sebagai Apsari. Dalam fragmen teks kuna sunda jatiniskala atau kropak 374, Apsari adalah sebuah entitas yang diyakini oleh masyarakat peladang sunda sebagai pembantu para pohaci atau dewi kesuburan untuk membantu manusia pada setiap kegiatan yang erat hubungannya dengan tradisi ladang. Dalam konteks ini, dikenal juga 40 nama apsari yang mendiami kayu, batu, pohon, ranting, tanah, daun dan buah-buahan. Maka dariitu angklung menjelma sebagai jalan antara para apsari turun dari dunia atas yang dikenal sebagai Niskala ke dunia bawah yang disebut Sakala atau dunia yang didiami oleh manusia, tumbuhan, dan hewan.</p>
+        <ul>
+        <p>ANGKLUNG DOGDOG LOJOR</p>
+        <p>Aki Dai [50 tahun], seorang pelaku Angklung Dogdog Lojor mengatakan bahwa dirinya sudah menggeluti Kesenian ini hampir 40 tahun, dimulai sejak usia 10 tahun, ia sudah mulai ikut serta dalam kesenian ini. Setelah orang tuanya meninggal ia meneruskan Kesenian ini karena menurut penuturannya, di Kasepuhan Ciptagelar semua bekerja berdasarkan turunan; Turunan Paraji anaknya akan menjadi Paraji, turunan Juru Pantun maka anaknya akan menjadi Juru Pantun, termasuk Orang Tua Aki Dai yang dititipkan Kesenian Angklung Dogdog Lojor, maka Aki Dai harus bersiap untuk meneruskan menjadi pemain sekaligus pimpinan Angklung Dogdog Lojor. Ia mengatakan bahwa “Di dalam Tradisi bertani Khas Masyarakat Adat Pancer Pangawinan, peran Angklung ini sangat penting, bukan hanya sebagai pengiring dan pelepas jenuh ketika sedang menanam Padi, lebih dari itu kesenian ini termasuk dalam Ritual untuk membahagiakan Padi,” ujar Aki Dai.
+        <ul>
+        <p>ANGKLUNG DALAM RITUS PADI</p>
+        <p>Aki Dai menjelaskan bahwa ketika pemimpin Adat akan melaksanakan Ngaseuk/ menanam padi, maka tugas Aki Dai adalah menghaturkan Kidung permohonan maaf kepada semua Makhluk ciptaanNya dan juga Kidung permintaan agar acara Ngaseuk dapat berjalan dengan lancar lewat kidung yang dibawakannya seperti, Kidung Bale Agung, Kidung Samping Hideung, Kidung Oleng-oleng Papanganten dan Kidung Adulilang. Menurutnya, Padi adalah salahsatu Makhluk yang sama seperti Manusia, maka ketika Padi mulai diaseuk (ditanam) hingga dipipit (dipanen) Padi harus diperlakukan sama seperti manusia. Aki Dai mempercayai bahwa Getaran yang dihasilkan oleh Angklung dan Dogdog ini dapat membahagiakan Padi, “Seperti manusia yang senang dan bahagia ketika mendengar musik, padi pun sama,” ujar Aki Dai. Aki Dai menjelaskan bahwa angklung yang ia gunakan pada setiap ritual adalah angklung yang mempunyai 5 laras nada atau pentatonis, nada tersebut adalah hasil adaptasi dari gamelan sunda. Angklung pentatonis berada dalam frekuensi 1583.2 hingga 990.62 Hz yang dipercaya bisa mempercepat pertumbuhan stomata pada tanaman hal itu didasari dari beberapa jurnal ilmiah yang mengatakan pertumbuhan suatu tanaman bisa dilakukan secara maksimal apabila stomata dapat bekerja dengan baik untuk kemudian memberikan peningkatan nutrisi sehingga dapat melakukan fotosintesis. Di beberapa negara maju dikenal sonicbloom, sebuah teknologi yang memanfaatkan gelombang suara berfrekuensi tinggi untuk meningkatkan pertumbuhan tanaman. (TAMA)</p>
+    `,
+        day: 28,
+        month: 'Juli',
+        year: 2025,
+        image: 'assets/blog/angklung.jpg',
+        category: 'STORY'
+    },
 ];
