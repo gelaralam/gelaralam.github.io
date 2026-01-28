7646383bc878179d843fc05f66ceaab6598ca587
nedisopian <143398477+nedisopian@users.noreply.github.com>
2026-01-28 17:45:00 +0700

    sync: js/blog-data.js

--- js/blog-data.js
+++ js/blog-data.js
@@ -70,17 +70,5 @@ const blogData = [
         "image": "assets/blog/kampung-padi.jpg",
         "video_url": "",
         "category": "STORY"
-    },
-    {
-        "id": 7,
-        "title": "Ngahiras di Kampung Adat Ciptarasa",
-        "description": "Ngahiras di Kampung Adat Ciptarasa. (A Dewan)",
-        "content": "\u003cp\u003eNgahiras di Kampung Adat Ciptarasa. (A Dewan)\u003c/p\u003e",
-        "day": 28,
-        "month": "Januari",
-        "year": 2026,
-        "image": "https://raw.githubusercontent.com/gelaralam/img/main/2026/01/2581abcd-e161-4e73-be9f-d0bae7cb2758.PNG",
-        "video_url": "https://youtube.com/shorts/HDrBT2Paf0U?si=OnsVQK5EheH6-3LD",
-        "category": "PERTANIAN"
     }
 ];
