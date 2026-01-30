603f595bace428819679fe7377adcf637447ef01
nedisopian <143398477+nedisopian@users.noreply.github.com>
2026-01-30 22:32:39 +0700

    sync: js/testimonial-data.js

--- js/testimonial-data.js
+++ js/testimonial-data.js
@@ -12,5 +12,12 @@ const testimonialData = [
         "text": "Berkunjung ke  Kasepuhan Gelar Alam bikin dada sesak haru bercampur syukur.\n\nDi tengah dunia yang makin berisik dan serba pamer, warga Kasepuhan hidup begitu tenang, sederhana, tanpa banyak suara. Menjalani hidup penuh hormat pada alam, gotong royong, syukur yang tulus semuanya terasa murni, hangat, dan dalam.\n\nRasanya seperti diingatkan pelan: kebahagiaan itu nggak perlu ribut, nggak perlu mewah. Cukup selaras dengan alam, saling jaga, dan bersyukur atas apa yang diberi.\n\nHati langsung adem, rindu ingin ke sana, merasakan langsung kedamaian itu.",
         "photo": "https://raw.githubusercontent.com/gelaralam/img/main/2026/01/0807e955-3a55-479f-8856-e2b0b48e8ecf.jpeg",
         "stars": 5
+    },
+    {
+        "name": "Kiki SN",
+        "origin": "Bandung",
+        "text": "Kalau dari Bandung sebenernya destinasi yang sangat lengkap pantai lewat, offroad oke, hutan ada dan tiba-tiba kayak nemu surga yang tersembunyi ditengah hutan...respect dijaman sekarang masih ada yang sangat memelihara ada Sunda...",
+        "photo": "https://raw.githubusercontent.com/gelaralam/img/main/2026/01/c4eb1155-f33a-448e-beb3-76ab5fd9bd9a.jpg",
+        "stars": 5
     }
 ];
