48ff49af43cd8a476990ad7717e176a3b738e489
nedisopian <143398477+nedisopian@users.noreply.github.com>
2026-01-30 22:32:20 +0700

    sync: js/testimonial-data.js

--- js/testimonial-data.js
+++ js/testimonial-data.js
@@ -5,5 +5,12 @@ const testimonialData = [
         "text": "Salah satu pengalaman paling unik yang pernah saya dapatkan! Dapat melihat keseharian warga adat dan kelestarian budaya yang tetap terjaga. Tidak lupa terimakasih untuk seluruh pihak yang telah membantu merealisasikan kunjungan saat itu!",
         "photo": "https://raw.githubusercontent.com/gelaralam/img/main/2026/01/9e8d2c46-298f-4f01-a035-831e8ec4dafe.jpeg",
         "stars": 5
+    },
+    {
+        "name": "Hananto",
+        "origin": "Temanggung",
+        "text": "Berkunjung ke  Kasepuhan Gelar Alam bikin dada sesak haru bercampur syukur.\n\nDi tengah dunia yang makin berisik dan serba pamer, warga Kasepuhan hidup begitu tenang, sederhana, tanpa banyak suara. Menjalani hidup penuh hormat pada alam, gotong royong, syukur yang tulus semuanya terasa murni, hangat, dan dalam.\n\nRasanya seperti diingatkan pelan: kebahagiaan itu nggak perlu ribut, nggak perlu mewah. Cukup selaras dengan alam, saling jaga, dan bersyukur atas apa yang diberi.\n\nHati langsung adem, rindu ingin ke sana, merasakan langsung kedamaian itu.",
+        "photo": "https://raw.githubusercontent.com/gelaralam/img/main/2026/01/0807e955-3a55-479f-8856-e2b0b48e8ecf.jpeg",
+        "stars": 5
     }
 ];
