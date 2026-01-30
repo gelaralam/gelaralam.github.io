38a467b95119bc4bc2c1144b322d4a6a3016036d
nedisopian <143398477+nedisopian@users.noreply.github.com>
2026-01-30 22:16:03 +0700

    sync: js/testimonial-data.js

--- js/testimonial-data.js
+++ js/testimonial-data.js
@@ -1,1 +1,9 @@
-const testimonialData = null;
+const testimonialData = [
+    {
+        "name": "Fahril V",
+        "origin": "Bandung",
+        "text": "Salah satu pengalaman paling unik yang pernah saya dapatkan! Dapat melihat keseharian warga adat dan kelestarian budaya yang tetap terjaga. Tidak lupa terimakasih untuk seluruh pihak yang telah membantu merealisasikan kunjungan saat itu!",
+        "photo": "https://raw.githubusercontent.com/gelaralam/img/main/2026/01/9e8d2c46-298f-4f01-a035-831e8ec4dafe.jpeg",
+        "stars": 5
+    }
+];
