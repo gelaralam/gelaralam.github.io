e17f3b6f9b66a07be33468f744521f75231d39ef
nedisopian <143398477+nedisopian@users.noreply.github.com>
2026-01-30 22:10:16 +0700

    sync: js/testimonial-data.js

--- js/testimonial-data.js
+++ js/testimonial-data.js
@@ -1,1 +1,9 @@
-const testimonialData = null;
+const testimonialData = [
+    {
+        "name": "Fahril V",
+        "origin": "Bandung",
+        "text": "",
+        "photo": "https://raw.githubusercontent.com/gelaralam/img/main/2026/01/9e83b213-180a-475b-ab3d-e0c9f5c99a21.jpeg",
+        "stars": 5
+    }
+];
